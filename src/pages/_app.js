b3ded09a509048ee5112d9bb10376f17279ec2e2
aalidev <aalidev90@gmail.com>
2022-10-27 20:36:36 +0600

    install analytic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import "@styles/globals.scss";
 import { SnipcartProvider } from "use-snipcart/useSnipcart";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SnipcartProvider>
-      <Component {...pageProps} />
-    </SnipcartProvider>
+    <>
+      <SnipcartProvider>
+        <Component {...pageProps} />
+      </SnipcartProvider>
+      <Analytics />
+    </>
   );
 }
 
